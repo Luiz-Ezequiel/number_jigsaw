6ad76720d5640a0dad295ed67a3cd011f262dc2e
luiz <ezequiel.luizedu@gmail.com>
2025-04-03 00:32:48 -0300

    feat(board): add moving numbers functionality

--- main.go
+++ main.go
@@ -1,11 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type EmptySquare struct {
-	xCord int
 	yCord int
+	xCord int
 }
+
+func move(board [][]string, blankSquare EmptySquare, size int, direction string) ([][]string, EmptySquare, error) {
+	var newX, newY int  
+	switch direction {
+	case "s":
+		if blankSquare.yCord == 0 {
+			return board, blankSquare, fmt.Errorf("can't move down")
+		}
+		newY, newX = blankSquare.yCord - 1, blankSquare.xCord
+	case "w":
+		if blankSquare.yCord == size - 1 {
+			return board, blankSquare, fmt.Errorf("can't move up")
+		}
+		newY, newX = blankSquare.yCord + 1, blankSquare.xCord
+	case "d":
+		if blankSquare.xCord == 0 {
+			return board, blankSquare, fmt.Errorf("can't move left")
+		}
+		newY, newX = blankSquare.yCord, blankSquare.xCord - 1
+	case "a":
+		if blankSquare.xCord == size - 1 {
+			return board, blankSquare, fmt.Errorf("can't move right")
+		}
+		newY, newX = blankSquare.yCord, blankSquare.xCord + 1
+	default:
+		fmt.Println("Invalid direction")
+		return board, blankSquare, nil
+	}
+
+	board[blankSquare.yCord][blankSquare.xCord] = board[newY][newX]
+	board[newY][newX] = "-- "
+	blankSquare.yCord, blankSquare.xCord = newY, newX
+	return board, blankSquare, nil
+}
+
 
 func createBoard(size int) ([][]string, EmptySquare) {
 	// Initialize the 2D array board && set the blank square position
@@ -36,7 +73,47 @@ func displayBoard(matrix [][]string) string {
 }
 
 func main() {
-	board, blankSquare := createBoard(4)
+	size := 3
+	board, blankSquare := createBoard(size)
 	fmt.Print(displayBoard(board))
-	fmt.Print(board[blankSquare.xCord][blankSquare.yCord])
+	fmt.Println("___________________")
+	board, blankSquare, err := move(board, blankSquare, size, "s")
+	if err != nil{
+		fmt.Print(err)
+		return
+	}
+	fmt.Print(displayBoard(board))
+	fmt.Println("___________________")
+	board, blankSquare, err = move(board, blankSquare, size, "s")
+	if err != nil{
+		fmt.Print(err)
+		return
+	}
+	fmt.Print(displayBoard(board))
+	fmt.Println("___________________")
+	board, blankSquare, err = move(board, blankSquare, size, "s")
+	if err != nil{
+		fmt.Print(err)
+		return
+	}
+	fmt.Print(displayBoard(board))
+	fmt.Println("___________________")
+	board, blankSquare, err = move(board, blankSquare, size, "d")
+	if err != nil{
+		return
+	}
+	fmt.Print(displayBoard(board))
+	fmt.Println("___________________")
+	board, blankSquare, err = move(board, blankSquare, size, "a")
+	if err != nil{
+		return
+	}
+	fmt.Print(displayBoard(board))
+	fmt.Println("___________________")
+	board, blankSquare, err = move(board, blankSquare, size, "w")
+	if err != nil{
+		return
+	}
+	fmt.Print(displayBoard(board))
+	// fmt.Print(board[blankSquare.yCord-1][blankSquare.xCord])
 }
